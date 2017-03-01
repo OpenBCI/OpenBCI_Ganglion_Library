9c8e66153846d720cc481d782f84d8e21449b06c
andreaortuno <andreaortuno@users.noreply.github.com>
2017-03-01 12:05:37 -0500

    Create Honor_A_Loved_One.cpp

---
+++ OpenBCI_Ganglion_Library/Honor_A_Loved_One.cpp
@@ -0,0 +1,26 @@
+//==========================================================//
+//     OpenBCI Kickstarter Pledge - Honor A Loved One       //
+//==========================================================//
+
+/*
+Name Of Loved One To Be Honored -- Note About Honored Loved One
+C.R. Zimmerman & Family -- all the best
+Leonidas Hofmann -- Always bravely carrying on despite Tourette's and OCD. You rock!
+Silvia Escamilla -- Te admiro, por tu voluntad y por tu fuerza, por tu cariño y por tu alegría.
+Jillian Burns -- Gone to Young, Forever in our hearts.
+Grandma Wang -- My grandmother suffered a stroke in 2005 and this is dedicated to her.
+Kyle Scott Dickinson -- My awesome autistic brother, I'm also his personal care attendant at the north shore arc. -- He’s a great brother in fact he won't cry from pain yet only when I'm upset lol.
+Robert S. Patton (my Dad) -- My Dad's life was ended by Alzheimer's. Fortunately for him, the disease took its toll quickly, less than 5 years. For him, and so many others I know, knew, and will know I encourage the founders to continue their work.
+Ruth Turner -- You were always loved and are missed
+Mark Wiegand -- At some point in time and space, we make you walk again.
+Eleonora Pawlowska -- In memory of my beloved mother. I wish she was still here with all of us.
+Dr Frederick Fries -- RIP 2010. Love you dad.
+Leibniz -- Nothing is accomplished all at once, and it is one of my great maxims, and one of the most completely verified, that Nature makes no leaps: a maxim which I have called the law of continuity.
+Ubirakitan Maciel -- A person dedicated to make Neuroscience accessible to people
+A. Sterling -- My father survived a brain aneurysm 16 years ago He just turned 90!
+Broome Spiro -- Different in an excellent sort of way.
+Lauren S. -- Your depression doesn't need to define you
+Stephanie Y. -- Hope you found your dream.
+My dad -- Essential tremor
+Peter Beals -- artist, the kindest man I ever know
+*/
