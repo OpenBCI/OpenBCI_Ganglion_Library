2e3ba3a2c1bd3775baa893842b4c2f473c96bf48
Richard Waltman <retiutut@icloud.com>
2019-01-18 11:56:55 -0600

    Revert "Revert "Apply Fix for Z axis #14 without multiplication""
    This reverts commit f0a241965fe61aac7ec5ed86fd0a5d5f8732de7e.

--- OpenBCI_Ganglion_Library.cpp
+++ OpenBCI_Ganglion_Library.cpp
@@ -333,7 +333,7 @@ void OpenBCI_Ganglion::sendCompressedPacket18() {
   if(useAccel){
     if(ringBufferLevel%10 == 1){ radioBuffer[19] = axisData[1]; } // Apply fix for #14
     if(ringBufferLevel%10 == 2){ radioBuffer[19] = axisData[0]; } // Swap X and Y axes
-    if(ringBufferLevel%10 == 3){ radioBuffer[19] = axisData[2] * -1; } // Invert Z axis
+    if(ringBufferLevel%10 == 3){ radioBuffer[19] = -axisData[2]; } // Invert Z axis
   } else if(useAux){
     if(ringBufferLevel%10 == 1){ radioBuffer[19] = auxData[0]; }
     if(ringBufferLevel%10 == 2){ radioBuffer[19] = auxData[1]; }
